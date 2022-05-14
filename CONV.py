0ece3027bcd1719d813df41fc76229839ea76e8e
jackson <zfng23@hotmail.com>
2022-05-14 15:42:28 +0800

    reorganized folders and added README.txt

--- CONV.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import Extension, setup
-from Cython.Build import cythonize
-
-# define an extension that will be cythonized and compiled
-ext1 = Extension(name="CFNS", sources=["CFNS.pyx"])
-setup(ext_modules=cythonize(ext1))
-
-# command to build extension is 'python3 CONV.py build_ext --inplace'
